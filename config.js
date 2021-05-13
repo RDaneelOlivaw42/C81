206ba0dbdb030c52cc0d384ba0a74d62677a956e
Asmi Rao <seldonbeeblebrox42@gmail.com>
2021-05-13 18:44:38 +0530

    C78

--- config.js
+++ config.js
@@ -2,17 +2,14 @@ import firebase from 'firebase';
 require('@firebase/firestore')
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBJsTqwL8yjTWW8mrCWgrBird7HI944SIw",
-    authDomain: "book-santa.firebaseapp.com",
-    databaseURL: "https://book-santa.firebaseio.com",
-    projectId: "book-santa",
-    storageBucket: "book-santa.appspot.com",
-    messagingSenderId: "546422247592",
-    appId: "1:546422247592:web:a82af183e05f978755f29d",
-    measurementId: "G-MLCQNSGDLH"
-  };
-  // Initialize Firebase
-  
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyBqTLZqFO03lukVtXSug5kAM6MBsA0nuiE",
+  authDomain: "book-santa-c77-50ba2.firebaseapp.com",
+  projectId: "book-santa-c77-50ba2",
+  storageBucket: "book-santa-c77-50ba2.appspot.com",
+  messagingSenderId: "526190980388",
+  appId: "1:526190980388:web:d56da538b48a889c8997b1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
   export default firebase.firestore();
